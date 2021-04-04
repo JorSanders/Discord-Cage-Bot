803be6be784b18b7f4f830786501a9623eba5d5b
Jor Sanders <it@jor.dev>
2021-04-04 20:21:35 +0200

    non relative import

--- discord_cage_bot/tests/discord_cage_bot_test.py
+++ discord_cage_bot/tests/discord_cage_bot_test.py
@@ -2,7 +2,7 @@
 
 import unittest
 
-from ..src.discord_cage_bot import cage_related_message, string_contains_word
+from discord_cage_bot.src.discord_cage_bot import cage_related_message, string_contains_word
 
 
 class TestDiscordCageBot(unittest.TestCase):
