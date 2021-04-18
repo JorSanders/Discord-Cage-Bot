90997999d4a8279f6fa9606ab427be8fc51c08a6
Jorkol <it@jor.dev>
2021-04-18 13:53:59 +0200

    make cagify logic more advanced (#12)
    * make cagify logic more advanced
    
    * update unit test command

--- jorkol/discord_cage_bot/src/string_utils.py
+++ jorkol/discord_cage_bot/src/string_utils.py
@@ -5,6 +5,24 @@ def string_contains_word(text, word_list):
     return any(word in text for word in word_list)
 
 
+def is_to_be_verb(word):
+    word = strip_non_alpha(word).lower()
+    return word in ["be", "is", "am", "are", "was", "were"]
+
+
+def is_cage(word):
+    word = strip_non_alpha(word).lower()
+    return word in ["cage", "caged", "caging", "cagism"]
+
+
+def is_cagifyable(word):
+    return not (is_to_be_verb(word) or is_cage(word))
+
+
+def strip_non_alpha(text):
+    return "".join(char for char in text if char.isalpha())
+
+
 def cagify_string(text):
     words = text.split()
     word_count = len(words)
@@ -14,18 +32,41 @@ def cagify_string(text):
         cage_count = 1
 
     for _ in range(cage_count):
-        index = randrange(word_count)
 
-        cagification = "cage"
+        # 10 rolls to find a cagifyable word
+        for _ in range(10):
+            index = randrange(word_count)
+            if is_cagifyable(words[index]):
+                break
 
-        if words[index][0].isupper():
-            cagification = cagification.capitalize()
-
-        if words[index].endswith("."):
-            cagification += "."
-        elif words[index].endswith(","):
-            cagification += ","
-
-        words[index] = cagification
+        words[index] = cagify_word(words[index])
 
     return " ".join(words)
+
+
+def cagify_word(word):
+    cagification = "cage"
+
+    special_suffix = ""
+
+    for i in range(1, len(word) + 1):
+        if word[-i].isalpha():
+            break
+
+        special_suffix += word[-i]
+
+    word = strip_non_alpha(word)
+
+    if word.endswith("ed"):
+        cagification = "caged"
+    elif word.endswith("ing"):
+        cagification = "caging"
+    elif word.endswith("ism"):
+        cagification = "cagism"
+
+    if len(word) > 0 and word[0].isupper():
+        cagification = cagification.capitalize()
+
+    cagification += special_suffix
+
+    return cagification
--- jorkol/discord_cage_bot/tests/string_utils_test.py
+++ jorkol/discord_cage_bot/tests/string_utils_test.py
@@ -1,6 +1,14 @@
 import unittest
 
-from jorkol.discord_cage_bot.src.string_utils import string_contains_word, cagify_string
+from jorkol.discord_cage_bot.src.string_utils import (
+    string_contains_word,
+    cagify_string,
+    cagify_word,
+    is_to_be_verb,
+    is_cagifyable,
+    is_cage,
+    strip_non_alpha,
+)
 
 
 class TestStringUtils(unittest.TestCase):
@@ -10,9 +18,53 @@ class TestStringUtils(unittest.TestCase):
 
     def test_cagify_string(self):
         self.assertTrue("cage" in cagify_string("one two three four"))
-        self.assertEqual("Cage", cagify_string("One"))
-        self.assertEqual("cage.", cagify_string("one."))
-        self.assertEqual("cage,", cagify_string("one,"))
+        self.assertEqual("Cage", cagify_string("Be"))
+        self.assertEqual("Cage", cagify_string("Cage"))
+        # 1/1024 chance to fail this test
+        self.assertEqual("Be cage", cagify_string("Be happy"))
+        self.assertEqual("Cage!", cagify_string("Hello!"))
+
+    def test_cagify_word(self):
+        self.assertEqual("cage", cagify_word("one"))
+        self.assertEqual("Cage", cagify_word("One"))
+        self.assertEqual("cage.", cagify_word("one."))
+        self.assertEqual("caged", cagify_word("walked"))
+        self.assertEqual("caging", cagify_word("walking"))
+        self.assertEqual("cagism", cagify_word("elitism"))
+        self.assertEqual("cage", cagify_word("cage"))
+        self.assertEqual("cage!", cagify_word("!"))
+        self.assertEqual("cage", cagify_word(""))
+
+    def test_is_to_be_verb(self):
+        self.assertTrue(is_to_be_verb("be"))
+        self.assertTrue(is_to_be_verb("is"))
+        self.assertTrue(is_to_be_verb("am"))
+        self.assertTrue(is_to_be_verb("are"))
+        self.assertTrue(is_to_be_verb("was"))
+        self.assertTrue(is_to_be_verb("were"))
+        self.assertTrue(is_to_be_verb("Were"))
+        self.assertTrue(is_to_be_verb("were!"))
+        self.assertTrue(is_to_be_verb("#were"))
+        self.assertFalse(is_to_be_verb("drop"))
+
+    def test_is_cage(self):
+        self.assertTrue(is_cage("cage"))
+        self.assertTrue(is_cage("caged"))
+        self.assertTrue(is_cage("Cage"))
+        self.assertTrue(is_cage("cage!"))
+        self.assertFalse(is_cage("caustic"))
+
+    def test_is_cagifyable(self):
+        self.assertFalse(is_cagifyable("cage"))
+        self.assertFalse(is_cagifyable("is"))
+        self.assertTrue(is_cagifyable("caustic"))
+
+    def test_strip_non_alpha(self):
+        self.assertEqual("cage", strip_non_alpha("cage!"))
+        self.assertEqual("cage", strip_non_alpha(" cage"))
+        self.assertEqual("cage", strip_non_alpha("ca@ge"))
+        self.assertEqual("", strip_non_alpha("!"))
+        self.assertEqual("", strip_non_alpha(""))
 
 
 if __name__ == "__main__":
