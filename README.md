a0747e6339e97297e613738847f7c1ec4bf5ceb3
Jor Sanders <it@jor.dev>
2022-02-20 16:27:10 +0100

    add logging

--- README.md
+++ README.md
@@ -12,31 +12,35 @@ Uhm why? But go ahead make a suggestion or PR, i'll probably implement it.
 ## Usage
 The Cage bot requires a discord bot token set as env var. Get the bot token from [here](https://discord.com/developers/applications)
 ```shell
-export DISCORD_BOT_TOKEN="xxxxxxxxxxxxxxxx"
+export CAGE_BOT_TOKEN="xxxxxxxxxxxxxxxx"
 ```
 
 Start the bot natively
 ```shell
-DISCORD_BOT_TOKEN=xxxx python3 -m jorkol.discord_cage_bot.src.discord_cage_bot
+python -m jorkol.discord_cage_bot.src.discord_cage_bot
 ```
 
 Start the bot as container
 ```shell
-docker stop discord_cage_bot &> /dev/null; docker run -it -d --rm --name discord_cage_bot -e DISCORD_BOT_TOKEN ghcr.io/jorsanders/discord_cage_bot:latest
+docker stop discord_cage_bot &> /dev/null; docker run -it -d --rm --name discord_cage_bot -e CAGE_BOT_TOKEN ghcr.io/jorsanders/discord_cage_bot:latest
+```
+
+Start the bot as frewly build container
+```shell
+docker stop discord_cage_bot &> /dev/null; docker build . -t ghcr.io/jorsanders/discord_cage_bot:local && docker run -it --rm --name discord_cage_bot -e CAGE_BOT_TOKEN ghcr.io/jorsanders/discord_cage_bot:local
 ```
 
 ## Setup
-- Install python3 and pip3
-```shell
-sudo apt install python3-pip
-```
+- Install pyenv
+    - https://github.com/pyenv/pyenv
+    - https://github.com/pyenv/pyenv#automatic-installer
+
+
 - Install discord pip package
 ```shell
 pip3 install -r requirements.txt
 ```
 dev dependencies
 ```shell
-pip3 install black
-pip3 install flake8
-pip3 install pylint
+pip3 install -r dev.requirements.txt
 ```
--- jorkol/discord_cage_bot/src/discord_cage_bot.py
+++ jorkol/discord_cage_bot/src/discord_cage_bot.py
@@ -1,4 +1,8 @@
+import argparse
+import logging
 import os
+import sys
+
 from datetime import datetime
 
 import discord
@@ -14,23 +18,28 @@ from jorkol.discord_cage_bot.src.string_utils import cagify_string
 
 class DiscordCageClient(discord.Client):
     async def on_ready(self):
-        print("Logged in as")
-        print(self.user.name)
-        print(self.user.id)
-        print("------")
+        logging.info(
+            """Logged in as:
+            user: %s
+            id: %s""",
+            self.user.name,
+            self.user.id,
+        )
 
     async def on_message(self, message):
-        print(datetime.now().strftime("%d/%m/%y %H:%M:%S") + "  Message received")
+        logging.info(
+            "%s Message received", datetime.now().strftime("%d/%m/%y %H:%M:%S")
+        )
         if message.author.id == self.user.id:
-            print("Message send by this bot")
+            logging.info("Message detected by this bot")
             return
 
         if not in_whitelisted_channel(message):
-            print("Message channel not whitelisted")
+            logging.info("Message channel not whitelisted")
             return
 
         if is_yikes_message(message):
-            print("Message is Y I K E S")
+            logging.info("Message is Y I K E S")
             await message.add_reaction("🇾")
             await message.add_reaction("🇮")
             await message.add_reaction("🇰")
@@ -39,7 +48,7 @@ class DiscordCageClient(discord.Client):
             return
 
         if is_cage_quote_request(message):
-            print("Message is request for cage quote")
+            logging.info("Message is request for cage quote")
             await message.reply(
                 "You asked for a Cage quote? I shall deliver: \n"
                 + cagify_string(random_quote())
@@ -47,12 +56,12 @@ class DiscordCageClient(discord.Client):
             return
 
         if is_cage_insult(message):
-            print("Message is an insult")
+            logging.info("Message is an insult")
             await message.reply(insult_response())
             return
 
         if is_cage_related_message(message):
-            print("Message is C A G E")
+            logging.info("Message is C A G E")
             await message.add_reaction("🇨")
             await message.add_reaction("🇦")
             await message.add_reaction("🇬")
@@ -61,29 +70,41 @@ class DiscordCageClient(discord.Client):
 
 
 def insult_response():
-    return " ".join(
-        [
-            "What the fuck did you just fucking say about me, you little bitch?",
-            "I'll have you know I graduated top of my class in the  IMC Military Academy, and I've been involved in numerous secret raids on the SRS Militia, and I have over 300 confirmed kills.",
-            "I am trained in gorilla warfare and I'm the top Kraber player in Titanfall.",
-            "You are nothing to me but just another target.",
-            "I will wipe you the fuck out with precision the likes of which has never been seen before on this Kings Canyon, mark my fucking words.",
-            "You think you can get away with saying that shit to me over the Internet?",
-            "Think again, fucker. As we speak I am contacting my secret network of spies across the Syndicate and your IP is being traced right now so you better prepare for the ring, maggot.",
-            "The ring that wipes out the pathetic little thing you call your life.",
-            "You're fucking dead, kid.",
-            "I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that's just with my Mozambique.",
-            "Not only am I extensively trained in Mozambique,",
-            "but I have access to the entire arsenal of the  IMC and I will use it to its full extent to wipe your miserable ass off the face of the continent,",
-            "you little shit.",
-            'If only you could have known what unholy retribution your little "clever" comment was about to bring down upon you, maybe you would have held your fucking tongue.',
-            "But you couldn't, you didn't, and now you're paying the price, you goddamn idiot.",
-            "I will shit fury all over you and you will drown in it.",
-            "You're fucking dead, kiddo.",
-        ]
-    )
+    return """What the fuck did you just fucking say about me, you little bitch? \
+            I'll have you know I graduated top of my class in the  IMC Military Academy, and I've been involved in numerous secret raids on the SRS Militia, and I have over 300 confirmed kills. \
+            I am trained in gorilla warfare and I'm the top Kraber player in Titanfall. \
+            You are nothing to me but just another target. \
+            I will wipe you the fuck out with precision the likes of which has never been seen before on this Kings Canyon, mark my fucking words. \
+            You think you can get away with saying that shit to me over the Internet? \
+            Think again, fucker. As we speak I am contacting my secret network of spies across the Syndicate and your IP is being traced right now so you better prepare for the ring, maggot. \
+            The ring that wipes out the pathetic little thing you call your life. \
+            You're fucking dead, kid. \
+            I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that's just with my Mozambique. \
+            Not only am I extensively trained in Mozambique, \
+            but I have access to the entire arsenal of the  IMC and I will use it to its full extent to wipe your miserable ass off the face of the continent, \
+            you little shit. \
+            If only you could have known what unholy retribution your little "clever" comment was about to bring down upon you, maybe you would have held your fucking tongue. \
+            But you couldn't, you didn't, and now you're paying the price, you goddamn idiot. \
+            I will shit fury all over you and you will drown in it. \
+            You're fucking dead, kiddo. \
+        """
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--loglevel",
+        default="warning",
+        help="Provide logging level. Example --loglevel debug, default=warning",
+    )
+
+    args = parser.parse_args()
+
+    logging.basicConfig(level=args.loglevel.upper())
+
     discord_cage_client = DiscordCageClient()
-    discord_cage_client.run(os.environ.get("DISCORD_BOT_TOKEN"))
+    discord_token = os.environ.get("CAGE_BOT_TOKEN")
+    if discord_token == "":
+        logging.error("CAGE_BOT_TOKEN env var undefined")
+        sys.exit(1)
+    discord_cage_client.run(discord_token)
