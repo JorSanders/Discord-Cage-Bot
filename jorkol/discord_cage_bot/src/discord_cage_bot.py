b737dce320bb015a01ec70c6e339f4633810cdad
jorkol <it@jor.dev>
2022-12-18 02:26:15 +0100

    update to checkout v3

--- jorkol/discord_cage_bot/src/discord_cage_bot.py
+++ jorkol/discord_cage_bot/src/discord_cage_bot.py
@@ -40,23 +40,23 @@ client = discord.Client(intents=intents)
 
 def insult_response():
     return """What the fuck did you just fucking say about me, you little bitch? \
-            I'll have you know I graduated top of my class in the  IMC Military Academy, and I've been involved in numerous secret raids on the SRS Militia, and I have over 300 confirmed kills. \
-            I am trained in gorilla warfare and I'm the top Kraber player in Titanfall. \
-            You are nothing to me but just another target. \
-            I will wipe you the fuck out with precision the likes of which has never been seen before on this Kings Canyon, mark my fucking words. \
-            You think you can get away with saying that shit to me over the Internet? \
-            Think again, fucker. As we speak I am contacting my secret network of spies across the Syndicate and your IP is being traced right now so you better prepare for the ring, maggot. \
-            The ring that wipes out the pathetic little thing you call your life. \
-            You're fucking dead, kid. \
-            I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that's just with my Mozambique. \
-            Not only am I extensively trained in Mozambique, \
-            but I have access to the entire arsenal of the  IMC and I will use it to its full extent to wipe your miserable ass off the face of the continent, \
-            you little shit. \
-            If only you could have known what unholy retribution your little "clever" comment was about to bring down upon you, maybe you would have held your fucking tongue. \
-            But you couldn't, you didn't, and now you're paying the price, you goddamn idiot. \
-            I will shit fury all over you and you will drown in it. \
-            You're fucking dead, kiddo. \
-        """
+I'll have you know I graduated top of my class in the  IMC Military Academy, and I've been involved in numerous secret raids on the SRS Militia, and I have over 300 confirmed kills. \
+I am trained in gorilla warfare and I'm the top Kraber player in Titanfall. \
+You are nothing to me but just another target. \
+I will wipe you the fuck out with precision the likes of which has never been seen before on this Kings Canyon, mark my fucking words. \
+You think you can get away with saying that shit to me over the Internet? \
+Think again, fucker. As we speak I am contacting my secret network of spies across the Syndicate and your IP is being traced right now so you better prepare for the ring, maggot. \
+The ring that wipes out the pathetic little thing you call your life. \
+You're fucking dead, kid. \
+I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that's just with my Mozambique. \
+Not only am I extensively trained in Mozambique, \
+but I have access to the entire arsenal of the  IMC and I will use it to its full extent to wipe your miserable ass off the face of the continent, \
+you little shit. \
+If only you could have known what unholy retribution your little "clever" comment was about to bring down upon you, maybe you would have held your fucking tongue. \
+But you couldn't, you didn't, and now you're paying the price, you goddamn idiot. \
+I will shit fury all over you and you will drown in it. \
+You're fucking dead, kiddo. \
+"""
 
 
 @client.event
